a3a0edcd5636661f5b1b97e5266bb0b0a370ba2e
chenjg <chenjg@lovengame.com>
2024-12-27 16:55:03 +0800

    first

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bytehi/redis-cli-hook
+
+go 1.22.4
+
+require github.com/bytehi/resp v0.0.0-20241227040428-8ade9db7bd42 // indirect
---
+++ main.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+
+	"github.com/bytehi/resp"
+)
+
+func parse() (ip, port string, leftArgs []string) {
+	ip = "127.0.0.1"
+	port = "6379"
+	leftArgs = make([]string, 0, len(os.Args)-1)
+	for i := 1; i < len(os.Args); i++ {
+		if os.Args[i] == "-h" {
+			i++
+			ip = os.Args[i]
+		} else if os.Args[i] == "-p" {
+			i++
+			port = os.Args[i]
+		} else {
+			leftArgs = append(leftArgs, os.Args[i])
+		}
+	}
+	return
+}
+
+func main() {
+	ip, port, leftArgs := parse()
+	fmt.Println(os.Args, ip, port, leftArgs)
+	proxyPort, closer := startProxy(ip, port)
+	defer closer()
+	leftArgs = append([]string{"-p", proxyPort}, leftArgs...)
+	execRedisCli(leftArgs)
+}
+
+//start proxy to connect redis, and return proxy local listen port
+func startProxy(ip, port string) (string, func()) {
+	//random listen local port
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatalf("Error listening: %v", err)
+	}
+	_, proxyPort, err := net.SplitHostPort(listener.Addr().String())
+	if err != nil {
+		log.Fatalf("Error parsing address:", err.Error())
+	}
+	log.Println("Server started, listening on:", listener.Addr().String(), proxyPort)
+
+	go func() {
+		for {
+			clientConn, err := listener.Accept()
+			if err != nil {
+				log.Printf("Error accepting connection: %v", err)
+				continue
+			}
+
+			serverConn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", ip, port))
+			if err != nil {
+				log.Printf("Error connecting to server: %v", err)
+				clientConn.Close()
+				continue
+			}
+
+			//go forward(clientConn, serverConn)
+			//go forward(serverConn, clientConn)
+			go forwardOneByOne(clientConn, serverConn, reqHookFunc, ackHookFunc)
+		}
+	}()
+	return proxyPort, func() { listener.Close() }
+}
+
+func execRedisCli(args []string) {
+	fmt.Println("call redis-cli, args:", args)
+	cmd := exec.Command("redis-cli", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	if err := cmd.Start(); err != nil {
+		fmt.Println("Error starting command:", err.Error())
+		return
+	}
+	cmd.Wait()
+}
+
+//unused
+func forward(src io.ReadCloser, dst io.WriteCloser) {
+	defer src.Close()
+	defer dst.Close()
+
+	reader := bufio.NewReader(src)
+	for {
+		msg, err := resp.Parse(reader)
+		if err != nil {
+			log.Printf("Error parse resp: %v", err)
+			break
+		}
+		fmt.Println(msg)
+		bs, err := msg.Marshal()
+		if err != nil {
+			log.Printf("Error marshal resp: %v", err)
+			break
+		}
+		_, err = dst.Write(bs)
+		if err != nil {
+			log.Printf("Error forwarding data: %v", err)
+			break
+		}
+	}
+}
+
+func forwardOneByOne(src, dst net.Conn, reqHook ReqHookFunc, ackHook AckHookFunc) {
+	defer src.Close()
+	defer dst.Close()
+
+	srcReader := bufio.NewReader(src)
+	dstReader := bufio.NewReader(dst)
+	for {
+		msg, err := resp.Parse(srcReader)
+		if err != nil {
+			log.Printf("Error parse resp: %v", err)
+			break
+		}
+
+		msg = reqHook(msg)
+		bs, err := msg.Marshal()
+		if err != nil {
+			log.Printf("Error marshal resp: %v", err)
+			break
+		}
+		_, err = dst.Write(bs)
+		if err != nil {
+			log.Printf("Error forwarding data: %v", err)
+			break
+		}
+
+		response, err := resp.Parse(dstReader)
+		if err != nil {
+			log.Printf("Error parse response: %v", err)
+			break
+		}
+		response = ackHook(msg, response)
+		bs, err = response.Marshal()
+		if err != nil {
+			log.Printf("Error marshal response: %v", err)
+			break
+		}
+		_, err = src.Write(bs)
+		if err != nil {
+			log.Printf("Error forwarding response: %v", err)
+			break
+		}
+	}
+}
+
+type ReqHookFunc func(req *resp.RESP) *resp.RESP
+type AckHookFunc func(req, rsp *resp.RESP) *resp.RESP
+
+func reqHookFunc(req *resp.RESP) *resp.RESP {
+	fmt.Println("req", req)
+	return req
+}
+func ackHookFunc(req, ack *resp.RESP) *resp.RESP {
+	fmt.Println("ack", ack)
+	return ack
+}
